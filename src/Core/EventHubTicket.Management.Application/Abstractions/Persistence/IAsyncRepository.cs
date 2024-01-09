806be0f52656a04a31400b9f5d973279417472f6
Ismael Herreras <ihdominguez905@gmail.com>
2024-01-09 18:59:57 -0400

    Add Application Core and abstractions for repositories.

---
+++ src/Core/EventHubTicket.Management.Application/Abstractions/Persistence/IAsyncRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace EventHubTicket.Management.Application.Abstractions.Persistence
+{
+    public interface IAsyncRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(Guid id);
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<T> CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ src/Core/EventHubTicket.Management.Application/Abstractions/Persistence/ICategoryRepository.cs
@@ -0,0 +1,8 @@
+﻿using EventHubTicket.Management.Domain.Entities;
+
+namespace EventHubTicket.Management.Application.Abstractions.Persistence
+{
+    public interface ICategoryRepository : IAsyncRepository<Category>
+    {
+    }
+}
