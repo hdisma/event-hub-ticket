ca168d58d5501b43f73c900fa7a0f6ac1e9e5a08
Ismael Herreras <ihdominguez905@gmail.com>
2024-03-08 22:58:04 -0400

    Add CreateEventCommandValidator class as part of the CreateEvent command

---
+++ src/Core/EventHubTicket.Management.Application/Features/Events/Commands/CreateEvent/CreateEventCommandValidator.cs
@@ -0,0 +1,39 @@
+﻿using EventHubTicket.Management.Application.Abstractions.Persistence;
+using FluentValidation;
+
+namespace EventHubTicket.Management.Application.Features.Events.Commands.CreateEvent
+{
+    public class CreateEventCommandValidator : AbstractValidator<CreateEventCommand>
+    {
+        public readonly IEventRepository _eventRepository;
+
+        public CreateEventCommandValidator(IEventRepository eventRepository)
+        {
+            _eventRepository = eventRepository;
+
+            RuleFor(p => p.Name)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters");
+
+            RuleFor(p => p.Date)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .GreaterThan(DateTime.Now);
+
+            RuleFor(c => c)
+                .MustAsync(EventNameAndDateUnique)
+                .WithMessage("An event with the same name and the same date already exists.");
+
+            RuleFor(p => p.Price)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .GreaterThan(0);
+        }
+
+        private async Task<bool> EventNameAndDateUnique(CreateEventCommand command,
+            CancellationToken cancellationToken)
+        {
+            return await _eventRepository.IsEventNameAndDateUnique(command.Name, command.Date);
+        }
+    }
+}
