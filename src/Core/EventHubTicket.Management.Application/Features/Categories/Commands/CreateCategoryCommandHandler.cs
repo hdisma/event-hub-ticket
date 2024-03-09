18139cef75e1d600a97e056b8ebfa5fa1f533eb2
Ismael Herreras <ihdominguez905@gmail.com>
2024-03-08 23:04:07 -0400

    Follows commit 7cf828332b280692b46a4fc5617c1e61b851bec6 :snake:
    I forgot to include these files in the previous commit :crying_cat_face:

---
+++ src/Core/EventHubTicket.Management.Application/Features/Categories/Commands/CreateCategoryCommandHandler.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using EventHubTicket.Management.Application.Abstractions.Persistence;
+using EventHubTicket.Management.Application.Dtos;
+using EventHubTicket.Management.Domain.Entities;
+using MediatR;
+
+namespace EventHubTicket.Management.Application.Features.Categories.Commands
+{
+    public class CreateCategoryCommandHandler : IRequestHandler<CreateCategoryCommand, CreateCategoryCommandResponse>
+    {
+        private readonly IAsyncRepository<Category> _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CreateCategoryCommandHandler(IAsyncRepository<Category> categoryRepository,
+            IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<CreateCategoryCommandResponse> Handle(CreateCategoryCommand request,
+            CancellationToken cancellationToken)
+        {
+            var createCategoryCommandResponse = new CreateCategoryCommandResponse();
+            var validator = new CreateCategoryCommandValidator();
+            var validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+            if (validationResult.Errors.Any())
+            {
+                createCategoryCommandResponse.Success = false;
+                createCategoryCommandResponse.ValidationErrors = new();
+
+                foreach (var error in validationResult.Errors)
+                {
+                    createCategoryCommandResponse.ValidationErrors.Add(error.ErrorMessage);
+                }
+            }
+
+            if (createCategoryCommandResponse.Success)
+            {
+                var category = new Category { Name = request.Name };
+
+                category = await _categoryRepository.CreateAsync(category);
+                createCategoryCommandResponse.Category = _mapper.Map<CreateCategoryDto>(category);
+            }
+
+            return createCategoryCommandResponse;
+        }
+    }
+}
---
+++ src/Core/EventHubTicket.Management.Application/Features/Categories/Commands/CreateCategoryCommandResponse.cs
@@ -0,0 +1,12 @@
+﻿using EventHubTicket.Management.Application.Dtos;
+using EventHubTicket.Management.Application.Responses;
+
+namespace EventHubTicket.Management.Application.Features.Categories.Commands
+{
+    public class CreateCategoryCommandResponse : BaseResponse
+    {
+        public CreateCategoryCommandResponse() : base() { }
+
+        public CreateCategoryDto Category { get; set; } = default!;
+    }
+}
