04b2c5c001036bf18697d85c64d232448da78d16
Ismael Herreras <ihdominguez905@gmail.com>
2024-03-08 22:54:01 -0400

    Add custom exceptions to Application layer

---
+++ src/Core/EventHubTicket.Management.Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,9 @@
+﻿namespace EventHubTicket.Management.Application.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ src/Core/EventHubTicket.Management.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation.Results;
+
+namespace EventHubTicket.Management.Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public List<string> ValidationErrors { get; set; }
+
+        public ValidationException(ValidationResult validationResult)
+        {
+            ValidationErrors = new();
+
+            foreach (var error in validationResult.Errors)
+            {
+                validationResult.Errors.Add(error);
+            }
+        }
+    }
+}
