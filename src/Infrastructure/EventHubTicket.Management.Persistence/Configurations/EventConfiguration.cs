c5cede1f1a21a57404b788262ae0fbaf5e8f9b65
Ismael Herreras <ihdominguez905@gmail.com>
2024-03-09 19:42:48 -0400

    Add EventHubTicketDbContext class and EventConfiguration class

---
+++ src/Infrastructure/EventHubTicket.Management.Persistence/Configurations/EventConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using EventHubTicket.Management.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EventHubTicket.Management.Persistence.Configurations
+{
+    public class EventConfiguration : IEntityTypeConfiguration<Event>
+    {
+        public void Configure(EntityTypeBuilder<Event> builder)
+        {
+            builder.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+    }
+}
---
+++ src/Infrastructure/EventHubTicket.Management.Persistence/DbContexts/EventHubTicketDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EventHubTicket.Management.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventHubTicket.Management.Persistence.DbContexts
+{
+    public class EventHubTicketDbContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public EventHubTicketDbContext(DbContextOptions<EventHubTicketDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
