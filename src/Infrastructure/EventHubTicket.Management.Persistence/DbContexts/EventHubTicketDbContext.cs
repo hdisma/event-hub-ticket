f91835e91427b486aaefc38c954a604be3f71949
Ismael Herreras <ihdominguez905@gmail.com>
2024-03-09 19:56:51 -0400

    :seedling: Update EventHubTicketDbContext
    Add OnModelCreating override to seed data in the database through migration. :seedling:

--- src/Infrastructure/EventHubTicket.Management.Persistence/DbContexts/EventHubTicketDbContext.cs
+++ src/Infrastructure/EventHubTicket.Management.Persistence/DbContexts/EventHubTicketDbContext.cs
@@ -13,5 +13,175 @@ namespace EventHubTicket.Management.Persistence.DbContexts
             : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(EventHubTicketDbContext).Assembly);
+
+            // Seed data through migration
+            var concertId = Guid.Parse("{7369722A-BD6C-4C65-ADC2-4E808FEEF74A}");
+            var musicalId = Guid.Parse("{0A326E93-C86B-49CE-8EA3-2D1964B26341}");
+            var playId = Guid.Parse("{4B546D73-1872-4842-B706-00993F0FC74B}");
+            var conferenceId = Guid.Parse("{10D9DCC8-40B0-421B-BFDB-CED634F32A6E}");
+
+            modelBuilder.Entity<Category>().HasData(new Category
+            {
+                Id = concertId,
+                Name = "Concerts"
+            });
+
+            modelBuilder.Entity<Category>().HasData(new Category
+            {
+                Id = musicalId,
+                Name = "Musicals"
+            });
+
+            modelBuilder.Entity<Category>().HasData(new Category
+            {
+                Id = playId,
+                Name = "Plays"
+            });
+
+            modelBuilder.Entity<Category>().HasData(new Category
+            {
+                Id = conferenceId,
+                Name = "Conferences"
+            });
+
+            modelBuilder.Entity<Event>().HasData(new Event
+            {
+                Id = Guid.Parse("{EE272F8B-6096-4CB6-8625-BB4BB2D89E8B}"),
+                Name = "John Egbert Live",
+                Price = 65,
+                Organizer = "John Egbert",
+                Date = DateTime.Now.AddMonths(6),
+                Description = "Join John for his farwell tour across 15 continents. John really needs no introduction since he has already mesmerized the world with his banjo.",
+                ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/FirstRowTickets/banjo.jpg",
+                CategoryId = concertId
+            });
+
+            modelBuilder.Entity<Event>().HasData(new Event
+            {
+                Id = Guid.Parse("{3448D5A4-0F72-4DD7-BF15-C14A46B26C00}"),
+                Name = "The State of Affairs: Michael Live!",
+                Price = 85,
+                Organizer = "Michael Johnson",
+                Date = DateTime.Now.AddMonths(9),
+                Description = "Michael Johnson doesn't need an introduction. His 25 concert across the globe last year were seen by thousands. Can we add you to the list?",
+                ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/FirstRowTickets/michael.jpg",
+                CategoryId = concertId
+            });
+
+            modelBuilder.Entity<Event>().HasData(new Event
+            {
+                Id = Guid.Parse("{B419A7CA-3321-4F38-BE8E-4D7B6A529319}"),
+                Name = "Clash of the DJs",
+                Price = 85,
+                Organizer = "DJ 'The Mike'",
+                Date = DateTime.Now.AddMonths(4),
+                Description = "DJs from all over the world will compete in this epic battle for eternal fame.",
+                ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/FirstRowTickets/dj.jpg",
+                CategoryId = concertId
+            });
+
+            modelBuilder.Entity<Event>().HasData(new Event
+            {
+                Id = Guid.Parse("{62787623-4C52-43FE-B0C9-B7044FB5929B}"),
+                Name = "Spanish guitar hits with Manuel",
+                Price = 25,
+                Organizer = "Manuel Santinonisi",
+                Date = DateTime.Now.AddMonths(4),
+                Description = "Get on the hype of Spanish Guitar concerts with Manuel.",
+                ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/FirstRowTickets/guitar.jpg",
+                CategoryId = concertId
+            });
+
+            modelBuilder.Entity<Event>().HasData(new Event
+            {
+                Id = Guid.Parse("{1BABD057-E980-4CB3-9CD2-7FDD9E525668}"),
+                Name = "Techorama 2021",
+                Price = 400,
+                Organizer = "Many",
+                Date = DateTime.Now.AddMonths(10),
+                Description = "The best tech conference in the world",
+                ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/FirstRowTickets/conf.jpg",
+                CategoryId = conferenceId
+            });
+
+            modelBuilder.Entity<Event>().HasData(new Event
+            {
+                Id = Guid.Parse("{ADC42C09-08C1-4D2C-9F96-2D15BB1AF299}"),
+                Name = "To the Moon and Back",
+                Price = 135,
+                Organizer = "Nick Sailor",
+                Date = DateTime.Now.AddMonths(8),
+                Description = "The critics are over the moon and so will you after you've watched this sing and dance extravaganza written by Nick Sailor, the man from 'My dad and sister'.",
+                ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/FirstRowTickets/musical.jpg",
+                CategoryId = musicalId
+            });
+
+            modelBuilder.Entity<Order>().HasData(new Order
+            {
+                Id = Guid.Parse("{7E94BC5B-71A5-4C8C-BC3B-71BB7976237E}"),
+                Total = 400,
+                IsPaid = true,
+                OrderPlaced = DateTime.Now,
+                UserId = Guid.Parse("{A441EB40-9636-4EE6-BE49-A66C5EC1330B}")
+            });
+
+            modelBuilder.Entity<Order>().HasData(new Order
+            {
+                Id = Guid.Parse("{86D3A045-B42D-4854-8150-D6A374948B6E}"),
+                Total = 135,
+                IsPaid = true,
+                OrderPlaced = DateTime.Now,
+                UserId = Guid.Parse("{AC3CFAF5-34FD-4E4D-BC04-AD1083DDC340}")
+            });
+
+            modelBuilder.Entity<Order>().HasData(new Order
+            {
+                Id = Guid.Parse("{771CCA4B-066C-4AC7-B3DF-4D12837FE7E0}"),
+                Total = 85,
+                IsPaid = true,
+                OrderPlaced = DateTime.Now,
+                UserId = Guid.Parse("{D97A15FC-0D32-41C6-9DDF-62F0735C4C1C}")
+            });
+
+            modelBuilder.Entity<Order>().HasData(new Order
+            {
+                Id = Guid.Parse("{3DCB3EA0-80B1-4781-B5C0-4D85C41E55A6}"),
+                Total = 245,
+                IsPaid = true,
+                OrderPlaced = DateTime.Now,
+                UserId = Guid.Parse("{4AD901BE-F447-46DD-BCF7-DBE401AFA203}")
+            });
+
+            modelBuilder.Entity<Order>().HasData(new Order
+            {
+                Id = Guid.Parse("{E6A2679C-79A3-4EF1-A478-6F4C91B405B6}"),
+                Total = 142,
+                IsPaid = true,
+                OrderPlaced = DateTime.Now,
+                UserId = Guid.Parse("{7AEB2C01-FE8E-4B84-A5BA-330BDF950F5C}")
+            });
+
+            modelBuilder.Entity<Order>().HasData(new Order
+            {
+                Id = Guid.Parse("{F5A6A3A0-4227-4973-ABB5-A63FBE725923}"),
+                Total = 40,
+                IsPaid = true,
+                OrderPlaced = DateTime.Now,
+                UserId = Guid.Parse("{F5A6A3A0-4227-4973-ABB5-A63FBE725923}")
+            });
+
+            modelBuilder.Entity<Order>().HasData(new Order
+            {
+                Id = Guid.Parse("{BA0EB0EF-B69B-46FD-B8E2-41B4178AE7CB}"),
+                Total = 116,
+                IsPaid = true,
+                OrderPlaced = DateTime.Now,
+                UserId = Guid.Parse("{7AEB2C01-FE8E-4B84-A5BA-330BDF950F5C}")
+            });
+        }
     }
 }
