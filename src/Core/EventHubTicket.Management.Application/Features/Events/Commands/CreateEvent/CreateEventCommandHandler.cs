dd79c3f1a5aaa8204d0e8c1b3ba9a9dc74f76d73
Ismael Herreras <ihdominguez905@gmail.com>
2024-03-09 23:35:31 -0400

    Update CreateEventCommandHandler.cs
    Add logic to send email when an event is created using the email service.

--- src/Core/EventHubTicket.Management.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
+++ src/Core/EventHubTicket.Management.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using EventHubTicket.Management.Application.Abstractions.Infrastructure;
 using EventHubTicket.Management.Application.Abstractions.Persistence;
 using EventHubTicket.Management.Application.Exceptions;
+using EventHubTicket.Management.Application.Models.Mail;
 using EventHubTicket.Management.Domain.Entities;
 using MediatR;
 
@@ -9,11 +11,15 @@ namespace EventHubTicket.Management.Application.Features.Events.Commands.CreateE
     public class CreateEventCommandHandler : IRequestHandler<CreateEventCommand, Guid>
     {
         private readonly IEventRepository _eventRepository;
+        private readonly IEmailService _emailService;
         private IMapper _mapper;
 
-        public CreateEventCommandHandler(IEventRepository eventRepository, IMapper mapper)
+        public CreateEventCommandHandler(IEventRepository eventRepository,
+            IEmailService emailService,
+            IMapper mapper)
         {
             _eventRepository = eventRepository;
+            _emailService = emailService;
             _mapper = mapper;
         }
 
@@ -28,6 +34,22 @@ namespace EventHubTicket.Management.Application.Features.Events.Commands.CreateE
             }
 
             var @event = _mapper.Map<Event>(request);
+            var email = new Email
+            {
+                To = "test@test.test",
+                Body = $"A new event was created: {request}",
+                Subject = "A new event was created"
+            };
+
+            try
+            {
+                await _emailService.SendEmailAsync(email);
+            }
+            catch (Exception)
+            {
+                // TODO: Log the exception
+                // An error when sending the email shouldn't stop this command execution.
+            }
 
             @event = await _eventRepository.CreateAsync(@event);
 
