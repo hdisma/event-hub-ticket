3b494df6036ae7421ea9a7239f1099329d3b72ea
Ismael Herreras <ihdominguez905@gmail.com>
2024-03-09 20:39:04 -0400

    Add EventRepository and CategoryRepository contract implementation.

---
+++ src/Infrastructure/EventHubTicket.Management.Persistence/Repositories/CategoryRepository.cs
@@ -0,0 +1,29 @@
+﻿using EventHubTicket.Management.Application.Abstractions.Persistence;
+using EventHubTicket.Management.Domain.Entities;
+using EventHubTicket.Management.Persistence.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace EventHubTicket.Management.Persistence.Repositories
+{
+    public class CategoryRepository : BaseRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(EventHubTicketDbContext eventHubTicketDbContext)
+            : base(eventHubTicketDbContext)
+        {
+        }
+
+        public async Task<List<Category>> GetCategoriesWithEvents(bool includePassedEvents)
+        {
+            var categories = _dbContext.Categories.Include(c => c.Events);
+
+            if (!includePassedEvents)
+            {
+                categories = (IIncludableQueryable<Category, ICollection<Event>?>)categories
+                    .Where(c => c.Events!.All(e => e.Date >= DateTime.Now));
+            }
+
+            return await categories.ToListAsync();
+        }
+    }
+}
---
+++ src/Infrastructure/EventHubTicket.Management.Persistence/Repositories/EventRepository.cs
@@ -0,0 +1,21 @@
+﻿using EventHubTicket.Management.Application.Abstractions.Persistence;
+using EventHubTicket.Management.Domain.Entities;
+using EventHubTicket.Management.Persistence.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventHubTicket.Management.Persistence.Repositories
+{
+    public class EventRepository : BaseRepository<Event>, IEventRepository
+    {
+        public EventRepository(EventHubTicketDbContext eventHubTicketDbContext)
+            : base(eventHubTicketDbContext)
+        {
+        }
+
+        public async Task<bool> IsEventNameAndDateUnique(string name, DateTime eventDate)
+        {
+            return await _dbContext.Events
+                .AnyAsync(e => e.Name == name && e.Date.Date.Equals(eventDate));
+        }
+    }
+}
