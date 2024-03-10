4d7d58374872f09d6b998042ce6160cb93b60bd0
Ismael Herreras <ihdominguez905@gmail.com>
2024-03-09 20:17:42 -0400

    Add IAsyncRepository implementation through BaseRepository
    Update IAsyncRepository.GetByIdAsync return type to be nullable.

--- src/Core/EventHubTicket.Management.Application/Abstractions/Persistence/IAsyncRepository.cs
+++ src/Core/EventHubTicket.Management.Application/Abstractions/Persistence/IAsyncRepository.cs
@@ -2,7 +2,7 @@
 {
     public interface IAsyncRepository<T> where T : class
     {
-        Task<T> GetByIdAsync(Guid id);
+        Task<T?> GetByIdAsync(Guid id);
         Task<IReadOnlyList<T>> GetAllAsync();
         Task<T> CreateAsync(T entity);
         Task UpdateAsync(T entity);
---
+++ src/Infrastructure/EventHubTicket.Management.Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,46 @@
+﻿using EventHubTicket.Management.Application.Abstractions.Persistence;
+using EventHubTicket.Management.Persistence.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventHubTicket.Management.Persistence.Repositories
+{
+    public abstract class BaseRepository<T> : IAsyncRepository<T> where T : class
+    {
+        protected readonly EventHubTicketDbContext _dbContext;
+
+        public BaseRepository(EventHubTicketDbContext eventHubTicketDbContext)
+        {
+            _dbContext = eventHubTicketDbContext;
+        }
+
+        public async Task<T> CreateAsync(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public virtual async Task<T?> GetByIdAsync(Guid id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
