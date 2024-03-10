b7fddc478cf7ad3c862695d3428c249dd5efe87f
Ismael Herreras <ihdominguez905@gmail.com>
2024-03-09 23:54:19 -0400

    Create DependencyInjection.cs
    Add DI class to register email service and initialize EmailSettings class.

---
+++ src/Infrastructure/EventHubTicket.Management.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using EventHubTicket.Management.Application.Abstractions.Infrastructure;
+using EventHubTicket.Management.Application.Models.Mail;
+using EventHubTicket.Management.Infrastructure.Mail;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EventHubTicket.Management.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
