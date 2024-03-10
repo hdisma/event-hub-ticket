310d33a466a888bcc83ef8fd2084c5fc4bf15ecf
Ismael Herreras <ihdominguez905@gmail.com>
2024-03-09 23:35:08 -0400

    Add Infrastructure project to infrastructure layer
    Add package Microsoft.Extensions.Options.ConfigurationExtensions v:8.0.0 :package:
    Add package Sendgrid v:9.29.2 :package:
    Add EmailService implementation class to infrastructure

---
+++ src/Infrastructure/EventHubTicket.Management.Infrastructure/Mail/EmailService.cs
@@ -0,0 +1,13 @@
+﻿using EventHubTicket.Management.Application.Abstractions.Infrastructure;
+using EventHubTicket.Management.Application.Models.Mail;
+
+namespace EventHubTicket.Management.Infrastructure.Mail
+{
+    public class EmailService : IEmailService
+    {
+        public Task<bool> SendEmailAsync(Email email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
