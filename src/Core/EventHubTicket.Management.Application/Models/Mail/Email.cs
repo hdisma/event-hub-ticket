4b9f8dffd2c1231a9d6dbe5683146226d62a3bf5
Ismael Herreras <ihdominguez905@gmail.com>
2024-03-09 22:50:47 -0400

    Add Email and EmailSettings models to application layer

---
+++ src/Core/EventHubTicket.Management.Application/Models/Mail/Email.cs
@@ -0,0 +1,9 @@
+﻿namespace EventHubTicket.Management.Application.Models.Mail
+{
+    public class Email
+    {
+        public required string To { get; set; }
+        public required string Subject { get; set; }
+        public required string Body { get; set; }
+    }
+}
---
+++ src/Core/EventHubTicket.Management.Application/Models/Mail/EmailSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace EventHubTicket.Management.Application.Models.Mail
+{
+    public class EmailSettings
+    {
+        public required string ApiKey { get; set; }
+        public required string FromAddress { get; set; }
+        public string FromName { get; set; } = string.Empty;
+    }
+}
