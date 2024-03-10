b7e9f012e6f737bd61cbdb689e0243d0de6ebb1a
Ismael Herreras <ihdominguez905@gmail.com>
2024-03-09 22:51:05 -0400

    Create IEmailService.cs
    Add IEmailService interface to application layer

---
+++ src/Core/EventHubTicket.Management.Application/Abstractions/Infrastructure/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using EventHubTicket.Management.Application.Models.Mail;
+
+namespace EventHubTicket.Management.Application.Abstractions.Infrastructure
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmailAsync(Email email);
+    }
+}
