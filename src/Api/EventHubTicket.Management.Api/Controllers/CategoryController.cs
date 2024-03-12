3d38aaa769b2751cded93a209bbb848aec52aafa
Ismael Herreras <ihdominguez905@gmail.com>
2024-03-11 23:49:51 -0400

    Update CategoryController.cs
    Add GetCategories endpoint and inject IMediator service.

--- src/Api/EventHubTicket.Management.Api/Controllers/CategoryController.cs
+++ src/Api/EventHubTicket.Management.Api/Controllers/CategoryController.cs
@@ -1,6 +1,24 @@
-﻿namespace EventHubTicket.Management.Api.Controllers
+﻿using EventHubTicket.Management.Application.Features.Categories.Queries.GetCategories;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventHubTicket.Management.Api.Controllers
 {
-    public class CategoryController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : ControllerBase
     {
+        public readonly IMediator _mediator;
+
+        public CategoryController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet(Name = "GetCategories")]
+        public async Task<IActionResult> GetCategories()
+        {
+            return Ok(await _mediator.Send(new GetCategoriesQuery()));
+        }
     }
 }
