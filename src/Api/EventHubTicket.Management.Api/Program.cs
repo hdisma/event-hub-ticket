59bfbf1d01f44909edc236b245a02be52127e7b8
Ismael Herreras <ihdominguez905@gmail.com>
2024-03-11 23:20:53 -0400

    Update Program.cs
    Add ResetDatabaseAsync to delete and create the database with the latest or pending migration.

--- src/Api/EventHubTicket.Management.Api/Program.cs
+++ src/Api/EventHubTicket.Management.Api/Program.cs
@@ -1,6 +1,8 @@
 using EventHubTicket.Management.Application;
 using EventHubTicket.Management.Infrastructure;
 using EventHubTicket.Management.Persistence;
+using EventHubTicket.Management.Persistence.DbContexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace EventHubTicket.Management.Api
 {
@@ -18,10 +20,33 @@ namespace EventHubTicket.Management.Api
 
             var app = builder.Build();
 
+            ResetDatabaseAsync(app).Wait();
+
             app.UseHttpsRedirection();
             app.MapControllers();
 
             app.Run();
+        }
+
+        public static async Task ResetDatabaseAsync(WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+
+            try
+            {
+                var context = scope.ServiceProvider.GetService<EventHubTicketDbContext>();
+
+                if (context is not null)
+                {
+                    await context.Database.EnsureDeletedAsync();
+                    await context.Database.MigrateAsync();
+                }
+            }
+            catch (Exception)
+            {
+                // TODO: Log exception here
+                throw;
+            }
         }
     }
 }
