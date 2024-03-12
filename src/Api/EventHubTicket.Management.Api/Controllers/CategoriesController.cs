c4bbd702527642159f0c565fa065efdd8c3a937c
Ismael Herreras <ihdominguez905@gmail.com>
2024-03-12 00:18:30 -0400

    Update CategoriesController.cs
    Add GetCategoriesWithEvents endpoint

--- src/Api/EventHubTicket.Management.Api/Controllers/CategoriesController.cs
+++ src/Api/EventHubTicket.Management.Api/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
 ﻿using EventHubTicket.Management.Application.Features.Categories.Queries.GetCategories;
+using EventHubTicket.Management.Application.Features.Categories.Queries.GetCategoriesWithEvents;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,6 +20,17 @@ namespace EventHubTicket.Management.Api.Controllers
         public async Task<IActionResult> GetCategories()
         {
             return Ok(await _mediator.Send(new GetCategoriesQuery()));
+        }
+
+        [HttpGet("events", Name = "GetCategoriesWithEvents")]
+        public async Task<IActionResult> GetCategoriesWithEvents([FromQuery] bool includeHistory)
+        {
+            var getCategoriesWithEventsQuery = new GetCategoriesWithEventsQuery()
+            {
+                IncludeHistory = includeHistory
+            };
+
+            return Ok(await _mediator.Send(getCategoriesWithEventsQuery));
         }
     }
 }
