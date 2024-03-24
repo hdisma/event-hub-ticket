b675bc8e0d0a546c95ce25816325d9b0a412b36c
Ismael Herreras <ihdominguez905@gmail.com>
2024-03-24 02:06:51 -0400

    Create GetEventsExportQueryHandler.cs

---
+++ src/Core/EventHubTicket.Management.Application/Features/Events/Queries/GetEventsExport/GetEventsExportQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using EventHubTicket.Management.Application.Abstractions.Infrastructure;
+using EventHubTicket.Management.Application.Abstractions.Persistence;
+using EventHubTicket.Management.Domain.Entities;
+using MediatR;
+
+namespace EventHubTicket.Management.Application.Features.Events.Queries.GetEventsExport
+{
+    public class GetEventsExportQueryHandler : IRequestHandler<GetEventsExportQuery,
+        EventExportFileViewModel>
+    {
+        private readonly IAsyncRepository<Event> _eventRepository;
+        private readonly IMapper _mapper;
+        private readonly ICsvExporter _csvExporter;
+
+        public GetEventsExportQueryHandler(
+            IAsyncRepository<Event> eventRepository,
+            IMapper mapper,
+            ICsvExporter csvExporter)
+        {
+            _eventRepository = eventRepository;
+            _mapper = mapper;
+            _csvExporter = csvExporter;
+        }
+
+        public async Task<EventExportFileViewModel> Handle(GetEventsExportQuery request, CancellationToken cancellationToken)
+        {
+            var events = await _eventRepository.GetAllAsync();
+
+            return new EventExportFileViewModel
+            {
+                FileName = $"{Guid.NewGuid()}.csv",
+                ContentType = "text/csv",
+                Data = _csvExporter.ExportToCsv(events)
+            };
+        }
+    }
+}
