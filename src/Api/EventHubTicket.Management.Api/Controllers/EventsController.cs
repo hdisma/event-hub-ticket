963ae8f086539fff163a7978e4dd50a86681bcb4
Ismael Herreras <ihdominguez905@gmail.com>
2024-03-24 00:56:46 -0400

    Update EventsController.cs
    Add GetEvents endpoint.

--- src/Api/EventHubTicket.Management.Api/Controllers/EventsController.cs
+++ src/Api/EventHubTicket.Management.Api/Controllers/EventsController.cs
@@ -1,8 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EventHubTicket.Management.Application.Features.Events.Queries.GetEvents;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EventHubTicket.Management.Api.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class EventsController : ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public EventsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet(Name = "GetEvents")]
+        public async Task<IActionResult> GetEvents()
+        {
+            return Ok(await _mediator.Send(new GetEventsQuery()));
+        }
     }
 }
