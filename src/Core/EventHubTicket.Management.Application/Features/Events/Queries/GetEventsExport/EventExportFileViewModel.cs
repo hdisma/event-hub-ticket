2769e89ea1016c8b1ae746cf7b6b49399eb8ff4a
Ismael Herreras <ihdominguez905@gmail.com>
2024-03-24 01:24:20 -0400

    Add GetEventsExportQuery class and EventExportFileViewModel class.
    This is going to be used to export all the events into a csv file.

---
+++ src/Core/EventHubTicket.Management.Application/Features/Events/Queries/GetEventsExport/EventExportFileViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EventHubTicket.Management.Application.Features.Events.Queries.GetEventsExport
+{
+    public class EventExportFileViewModel
+    {
+        public string FileName { get; set; } = string.Empty;
+        public string ContentType { get; set; } = string.Empty;
+        public byte[]? Data { get; set; }
+    }
+}
---
+++ src/Core/EventHubTicket.Management.Application/Features/Events/Queries/GetEventsExport/GetEventsExportQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace EventHubTicket.Management.Application.Features.Events.Queries.GetEventsExport
+{
+    public class GetEventsExportQuery : IRequest<EventExportFileViewModel>
+    {
+    }
+}
