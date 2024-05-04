0021aac37fe96bf23987258001a3b055eb7b4bd5
Ismael Herreras <ihdominguez905@gmail.com>
2024-05-04 00:55:59 -0400

    Create UpdateCategoryDto.cs

---
+++ src/Core/EventHubTicket.Management.Application/Dtos/UpdateCategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace EventHubTicket.Management.Application.Dtos
+{
+    public class UpdateCategoryDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
